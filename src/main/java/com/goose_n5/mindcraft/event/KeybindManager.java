2b4d603c8c4a8c845f1c95569b3bae4102578199
Romain Bareau <baromain.romain.barreau31@gmail.com>
2025-03-25 15:19:12 +0100

    feat(screen) : add ConfigScreen displaying questions

--- src/main/java/com/goose_n5/mindcraft/event/KeybindManager.java
+++ src/main/java/com/goose_n5/mindcraft/event/KeybindManager.java
@@ -1,5 +1,6 @@
 package com.goose_n5.mindcraft.event;
 
+import com.goose_n5.mindcraft.screen.ConfigScreen;
 import com.goose_n5.mindcraft.screen.MindCraftScreen;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -13,8 +14,10 @@ import org.lwjgl.glfw.GLFW;
 public class KeybindManager {
     public static final String KEYBIND_CATEGORY = "key.category.mindcraft";
     public static final String KEYBIND_OPEN_GUI = "key.mindcraft.open_gui";
+    public static final String KEYBIND_OPEN_CONFIG = "key.mindcraft.open_config";
 
     public static KeyBinding openGui;
+    public static KeyBinding openConfig;
 
     public static void registerKeyInputs() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
@@ -23,6 +26,12 @@ public class KeybindManager {
                 Screen currentScreen = MinecraftClient.getInstance().currentScreen;
                 MinecraftClient.getInstance().setScreen(
                         new MindCraftScreen(Text.empty(),currentScreen)
+                );
+            }else if (openConfig.wasPressed()) {
+                System.out.println("Open Config key was pressed");
+                Screen currentScreen = MinecraftClient.getInstance().currentScreen;
+                MinecraftClient.getInstance().setScreen(
+                        new ConfigScreen(Text.empty(),currentScreen)
                 );
             }
         });
@@ -33,6 +42,12 @@ public class KeybindManager {
                 KEYBIND_OPEN_GUI,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_K,
+                KEYBIND_CATEGORY
+        ));
+        openConfig = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEYBIND_OPEN_CONFIG,
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_C,
                 KEYBIND_CATEGORY
         ));
     }
