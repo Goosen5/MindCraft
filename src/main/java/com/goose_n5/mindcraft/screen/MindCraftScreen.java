f404be4a33221ed88d0e0d3040e89980e5b2c77a
Goosen5 <romain.barreau31@gmail.com>
2025-03-25 01:48:22 +0100

    feat(screen) : load questions from JSON file in MindCraftScreen

--- src/main/java/com/goose_n5/mindcraft/screen/MindCraftScreen.java
+++ src/main/java/com/goose_n5/mindcraft/screen/MindCraftScreen.java
@@ -1,16 +1,31 @@
 package com.goose_n5.mindcraft.screen;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Objects;
+
 public class MindCraftScreen extends Screen {
 
-    public Screen parent;
+    private Screen parent;
+    private List<Question> questions;
 
     public MindCraftScreen(Text title ,Screen parent) {
         super(title);
         this.parent = parent;
+        loadQuestions();
+    }
+
+    private static class Question{
+        String question;
+        List<String> answers;
+        int correct;
     }
 
     @Override
@@ -24,6 +39,14 @@ public class MindCraftScreen extends Screen {
         int textY = (this.height - this.textRenderer.fontHeight) / 2;
         context.drawText(this.textRenderer, "Screen", textX, textY, 0xFFFFFF, true);
         System.out.println("Screen");
+    }
+
+    private void loadQuestions(){
+        Gson gson = new Gson();
+        Type rewardListType = new TypeToken<List<Question>>(){}.getType();
+        InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("/assets/mindcraft/questions.json")));
+        questions = gson.fromJson(reader, rewardListType);
+        System.out.println(questions);
     }
 
     @Override
